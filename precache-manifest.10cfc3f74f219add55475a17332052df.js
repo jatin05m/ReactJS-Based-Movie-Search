9e680038e3e961f415474f8558cd47cd99eb6600
Jatin Malhotra <jatin05m@gmail.com>
2020-10-21 16:05:56 +0530

    Updates

---
+++ precache-manifest.10cfc3f74f219add55475a17332052df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b14ed1a1322b7656b767bd844ce76997",
+    "url": "/jatin05m/ReactJS-Based-Movie-Search.git/index.html"
+  },
+  {
+    "revision": "1df4a2e196cbb146475b",
+    "url": "/jatin05m/ReactJS-Based-Movie-Search.git/static/css/main.27be7390.chunk.css"
+  },
+  {
+    "revision": "76b052fb3654b9c75c43",
+    "url": "/jatin05m/ReactJS-Based-Movie-Search.git/static/js/2.64aa3eb9.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/jatin05m/ReactJS-Based-Movie-Search.git/static/js/2.64aa3eb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df4a2e196cbb146475b",
+    "url": "/jatin05m/ReactJS-Based-Movie-Search.git/static/js/main.2ee32342.chunk.js"
+  },
+  {
+    "revision": "cc7b03f770e9ceb2f01d",
+    "url": "/jatin05m/ReactJS-Based-Movie-Search.git/static/js/runtime-main.d39968f6.js"
+  }
+]);
